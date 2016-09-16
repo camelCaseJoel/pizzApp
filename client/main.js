eabdd6f54bf2d8346d191bfe28648e59038a8949
Joel Rivera <Joel Rivera>
2016-09-16 11:20:09 -0400

    Código de validación de pedidos para 'Mesas que existen'

--- client/main.js
+++ client/main.js
@@ -6,7 +6,19 @@ Template.hacerPedido.helpers({
 });
 
 Template.hacerPedido.events({
-	'click button'(event) {
-		//some code here...
+	'click button#nuevo_pedido'(event) {
+
+		//limpiar mensajes de error
+		$('#mensaje_error').html('');
+
+		const mesasExistentes = [1,2,3]; 
+		let numeroMesa = Number( $('#mesa').val() );
+		
+		//debugging:
+		//console.log( _.contains(mesasExistentes, numeroMesa) );
+
+		if( !(_.contains(mesasExistentes, numeroMesa)) ){
+			$('#mensaje_error').html('Esta mesa no existe');
+		}
 	}
 });
