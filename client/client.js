884c8b4b9b57f982e1e5655464d2dab120a42a28
Joel Rivera <tilyxxx@gmail.com>
2016-09-18 14:55:24 -0400

    login-box shows or dissapears when logged in or not

--- client/client.js
+++ client/client.js
@@ -10,7 +10,7 @@ Template.hacerPedido.helpers({
 });
 
 Template.hacerPedido.events({
-	'click button#nuevo_pedido'(event) {
+	'click button#nuevo_pedido'( event ) {
 		
 		//limpiar mensajes de error
 		$('#mensaje_error').html('');
@@ -24,17 +24,53 @@ Template.hacerPedido.events({
 });
 
 //==================================================
+//                LOGIN-BOX TEMPLATE 
+//==================================================
+Template.login_box.events({
+	'click button#login' (event){
+		let user = $('#name').val();
+		let pass = $('#password').val();
+		Meteor.loginWithPassword( user, pass , (error)=>{
+			if(error){
+				console.log( error.reason );
+			}
+		});
+	}
+});
+
+
+//==================================================
+//                USER-BOX TEMPLATE 
+//==================================================
+Template.user_box.events({
+	'click button#logout' ( event ){
+		Accounts.logout();
+	}
+});
+
+
+//==================================================
 //                CREATE-USER TEMPLATE 
 //==================================================
 Template.createUser.events({
-	'click button#create_user'(event){
+	'click button#create_user'( event ){
+		let userName = $('div.create_user_box #name').val();
+		let password = $('div.create_user_box #password').val();
+
 		let options = {
-			username: $('#name').val(),
-			password: $('#name').val(),
+			username: userName,
+			password: password,
 			role: $('#role').val()
 
 		}
 		console.log( options )
-		Accounts.createUser( options );
+
+		Accounts.createUser( options, (error)=>{
+			if(error){
+				console.log( error.reason );
+			}
+			
+		} );
 	}
 });
+
