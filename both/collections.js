c85d38172d8c6f134352856f782b4aefa7558f90
Joel Rivera <Joel Rivera>
2016-09-20 13:18:42 -0400

    UI html/css de 'order_box'(cajitas de órdenes)

--- both/collections.js
+++ both/collections.js
@@ -3,18 +3,22 @@ let Ordenes = new Mongo.Collection('ordenes');
 let Mesas = new Mongo.Collection('mesas');
 
 if(Meteor.isServer){
-	// =================== PUBLISH =================
+	// ==================== PUBLISH ===================
 	Meteor.publish('todasMesas',function(){
 		return Mesas.find();
 	});
 	Meteor.publish('products',()=>{
 		return Productos.find();
 	});
+	Meteor.publish('orders',()=>{
+		return Ordenes.find();
+	});
 }
 if(Meteor.isClient){
 	//================== SUBSCRIPTIONS==================
 	Meteor.subscribe('todasMesas');
 	Meteor.subscribe('products');
+	Meteor.subscribe('orders');
 }
 
 
